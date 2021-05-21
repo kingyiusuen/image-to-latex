e9ddfb121cc267a6fb7b829d76d66761c161d926
King Yiu Suen <kingyiusuen@gmail.com>
2021-05-21 08:11:20 -0500

    Change to CRNN for faster testing

--- tests/test_run_experiment.sh
+++ tests/test_run_experiment.sh
@@ -3,27 +3,14 @@ FAILURE=false
 
 python run_experiment.py \
   --data_class=FakeData \
-  --model_class=ResnetTransformer \
-  --resnet_num_layers=3 \
-  --tf_dim=128 \
-  --tf_fc_dim=256 \
-  --tf_num_layers=1 \
-  --max_output_length=30 \
-  --tf_nhead=1 \
+  --image_width=128 \
+  --image_height=32 \
+  --num_samples=128 \
+  --conv_dim=16 \
+  --model_class=CRNN \
   --max_epochs=2 \
+  --max_lr=0.01 \
 || FAILURE=true
-
-
-#python run_experiment.py \
-#  --data_class=FakeData \
-#  --image_width=128 \
-#  --image_height=32 \
-#  --num_samples=128 \
-#  --conv_dim=16 \
-#  --model_class=CRNN \
-#  --max_epochs=2 \
-#  --max_lr=0.01 \
-#|| FAILURE=true
 
 
 if [ "$FAILURE" = true ]; then
